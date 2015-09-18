18617eeadb0637bf70b7f05fcca9905dd6435100
Brian Elvesæter <brian.elvesater@sintef.no>
2015-09-18 12:53:15 +0200

    Added installation scripts.

---
+++ installation-scripts/install-proasense-storage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get --yes --force-yes install default-jdk
+sudo apt-get --yes --force-yes install nano
+sudo apt-get --yes --force-yes install maven
+sudo apt-get --yes --force-yes install git
+sudo apt-get --yes --force-yes install tomcat7
+sudo apt-get --yes --force-yes install -y mongodb-org
