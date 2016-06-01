779e37de0d091408e49bbd378bc6aff0712b90b5
Brian Elvesæter <brian.elvesater@sintef.no>
2016-06-01 04:04:25 +0300

    Updated Registry Service with JSON outputs.

--- storage-registry/src/main/java/net/modelbased/proasense/storage/fuseki/StorageRegistryFusekiService.java
+++ storage-registry/src/main/java/net/modelbased/proasense/storage/fuseki/StorageRegistryFusekiService.java
@@ -119,6 +119,41 @@ public class StorageRegistryFusekiService {
 
 
     @GET
+    @Path("/query/machine/list2")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response queryMachineList2()
+    {
+        String SPARQL_MACHINE_LIST = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#>\n" +
+                "PREFIX pssn: <http://www.sintef.no/pssn#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?x\n" +
+                "  WHERE {\n" +
+                "    ?subject rdfs:subClassOf+ pssn:Machine .\n" +
+                "    ?x rdf:type ?subject\n" +
+                "  }\n" +
+                "ORDER BY ASC (?x)";
+
+        QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SPARQL_ENDPOINT, SPARQL_MACHINE_LIST);
+        ResultSet results = qe.execSelect();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ResultSetFormatter.outputAsJSON(baos, results);
+
+        String resultsJson = baos.toString();
+        resultsJson = resultsJson.replaceAll("http://www.sintef.no/pssn#", "");
+
+        String result = resultsJson;
+
+        // Return HTTP response 200 in case of success
+        return Response.status(200).entity(result).build();
+    }
+
+
+    @GET
     @Path("/query/machine/properties")
     @Produces(MediaType.APPLICATION_JSON)
     public Response queryMachineProperties(
